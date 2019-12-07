9aabb2faff8ef4c65c0d5a738b8c17886e059b9b
Pesho420 <petar.nyagolov@leanplum.com>
2019-12-07 16:16:41 +0200

    header with all implementations

---
+++ cpp_ml/cpp_ml.h
@@ -0,0 +1,10 @@
+#ifndef CPP_ML_H
+#define CPP_ML_H
+
+#include "Sigmoid.cpp"
+#include "RandomNumberGenerator.cpp"
+#include "Xorshift.cpp"
+#include "Matrix.cpp"
+#include "NeuralNetwork.cpp"
+
+#endif
